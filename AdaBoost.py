2386a13116c9f8f793e2761b9719045bedfc588f
jackyang <ijackyang@163.com>
2020-02-03 17:31:08 +0800

    AdaBoost 三种分类器比较

--- AdaBoost.py
+++ AdaBoost.py
@@ -4,6 +4,9 @@ from sklearn.metrics import mean_squared_error
 from sklearn.datasets import load_boston
 from sklearn.ensemble import AdaBoostRegressor
 # 加载数据
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+
 data=load_boston()
 # 分割数据
 train_x, test_x, train_y, test_y = train_test_split(data.data, data.target, test_size=0.25, random_state=33)
@@ -14,3 +17,16 @@ pred_y = regressor.predict(test_x)
 mse = mean_squared_error(test_y, pred_y)
 print("房价预测结果 ", pred_y)
 print("均方误差 = ",round(mse,2))
+
+# 使用决策树回归模型
+dec_regressor=DecisionTreeRegressor()
+dec_regressor.fit(train_x,train_y)
+pred_y = dec_regressor.predict(test_x)
+mse = mean_squared_error(test_y, pred_y)
+print("决策树均方误差 = ",round(mse,2))
+# 使用KNN回归模型
+knn_regressor=KNeighborsRegressor()
+knn_regressor.fit(train_x,train_y)
+pred_y = knn_regressor.predict(test_x)
+mse = mean_squared_error(test_y, pred_y)
+print("KNN均方误差 = ",round(mse,2))
